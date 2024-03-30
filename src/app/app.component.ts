c3f39c2b6d615e67fdf86b2f352b532122f94f9a
Gabriel Soares <gabrielsoares.pe.br@gmail.com>
2024-03-30 14:37:37 -0300

    feat: Layout divider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { PresentationComponent } from './components/presentation/presentation.component';
 import { AboutComponent } from './components/about/about.component';
+import { DividerComponent } from './components/shared/divider/divider.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, PresentationComponent, AboutComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    PresentationComponent,
+    AboutComponent,
+    DividerComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
